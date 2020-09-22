9118d086681f1b6566ec4cf4623c41015e13def8
Erin Wills <ejw.data@gmail.com>
2020-09-21 23:45:15 -0500

    updated filter methods

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -10,42 +10,61 @@ generateTable(tableData);
 var filterButton = d3.select("#filter-btn");
 filterButton.on("click", function() {
     tableData = data;
-    var emptyFilter = true;
+    
+    // emptyFilter only needed for Filter Method #1
+    //var emptyFilter = true;
     var dateFilter = d3.select("#datetime").property("value");
     var cityFilter = d3.select("#city").property("value");
     var stateFilter = d3.select("#state").property("value");
     var countryFilter = d3.select("#country").property("value");
     var shapeFilter = d3.select("#shape").property("value");
 
-    if (dateFilter !==""){
-        tableData = tableData
-            .filter(tableData => tableData.datetime === dateFilter);
-        emptyFilter = false;
-    };
-    if (cityFilter !==""){
-        tableData = tableData
-            .filter(tableData => tableData.city === cityFilter);
-        emptyFilter = false;
-    };
-    if (stateFilter !==""){
-        tableData = tableData
-            .filter(tableData => tableData.state === stateFilter);
-        emptyFilter = false;
-    };
-    if (countryFilter !==""){
-        tableData = tableData
-            .filter(tableData => tableData.country === countryFilter);
-        emptyFilter = false;
-    };
-    if (shapeFilter !==""){
-        tableData = tableData
-            .filter(tableData => tableData.shape === shapeFilter);
-        emptyFilter = false;
-    };
-    if (emptyFilter){
-        tableData = data;
-    }
-    generateTable(tableData);
+    // // Filter Method #1
+    // if (dateFilter !==""){
+    //     tableData = tableData
+    //         .filter(tableData => tableData.datetime === dateFilter);
+    //     emptyFilter = false;
+    // };
+    // if (cityFilter !==""){
+    //     tableData = tableData
+    //         .filter(tableData => tableData.city === cityFilter);
+    //     emptyFilter = false;
+    // };
+    // if (stateFilter !==""){
+    //     tableData = tableData
+    //         .filter(tableData => tableData.state === stateFilter);
+    //     emptyFilter = false;
+    // };
+    // if (countryFilter !==""){
+    //     tableData = tableData
+    //         .filter(tableData => tableData.country === countryFilter);
+    //     emptyFilter = false;
+    // };
+    // if (shapeFilter !==""){
+    //     tableData = tableData
+    //         .filter(tableData => tableData.shape === shapeFilter);
+    //     emptyFilter = false;
+    // };
+    // if (emptyFilter){
+    //     tableData = data;
+    // }
+
+    // // Filter Method #2
+    // var tableData2 = tableData.filter(i => {if (dateFilter != "") {return i.datetime === dateFilter;} else {return true;}})
+    //                         .filter(i => {if (cityFilter != ""){ return i.city === cityFilter;} else {return true;}})
+    //                         .filter(i => {if (stateFilter != ""){return i.state === stateFilter;} else {return true;}})
+    //                         .filter(i => {if (countryFilter != ""){return i.country === countryFilter;} else {return true;}})
+    //                         .filter(i => {if (shapeFilter != ""){return i.shape === shapeFilter;} else {return true;}});
+
+    // Filter Method #2 Refined
+    var tableData2 = tableData.filter(i => {if (dateFilter) {return i.datetime === dateFilter;} else {return true;}})
+                            .filter(i => {if (cityFilter){ return i.city === cityFilter;} else {return true;}})
+                            .filter(i => {if (stateFilter){return i.state === stateFilter;} else {return true;}})
+                            .filter(i => {if (countryFilter){return i.country === countryFilter;} else {return true;}})
+                            .filter(i => {if (shapeFilter){return i.shape === shapeFilter;} else {return true;}});
+
+
+    generateTable(tableData2);
     
 });
 
